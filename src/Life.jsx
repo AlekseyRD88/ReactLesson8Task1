4e309acf6fd68a6f4f66dbb323026cf93cb9c93a
= <neymegen50@gmail.com>
2022-08-27 22:36:06 +0300

    message

--- src/Life.jsx
+++ src/Life.jsx
@@ -9,11 +9,14 @@ class Life extends Component {
   componentDidMount() {
     console.log('componentDidMount: API calls, subscriptions');
   }
-  shouldComponentUpdate() {
+  shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true;
   }
-  componentDidUpdate() {
+  componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+    return true;
+
   }
   componentWillUnmount() {
     console.log('cleanup before DOM related to component will be removed');
@@ -21,7 +24,7 @@ class Life extends Component {
 
   render() {
     console.log('return React element to build DOM');
-    return this.componentDidMount;
+    return <h1>These are my lifecycle methods</h1>;
   }
 }
 
